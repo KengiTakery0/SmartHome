570b762f40dcfb0acdcfeb8d046edd230e8d0019
Roman <74704965+KengiTakery0@users.noreply.github.com>
2023-09-14 13:05:29 +0300

    Create Project template

---
+++ SmartHomeHub/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartHomeHub
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SmartHomeHub/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartHomeHub
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
